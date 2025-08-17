ba0138ee8395ad271b033dd6288e6e2251837dc4
bebe-pirat <pirat.bebe@gmail.com>
2025-08-17 21:38:44 +0300

    clang-formatted

---
+++ apps/log_sender_additional/src/main.cpp
@@ -0,0 +1,24 @@
+#include "logger.h"
+#include <thread>
+
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    std::cerr << "Использование: " << argv[0] << " <порт> <ip адрес>\n";
+    return 1;
+  }
+
+  try {
+    unsigned short port = static_cast<unsigned short>(std::stoi(argv[1]));
+
+    SocketLogger logger(Severity_level::DEBUG, port, argv[2]);
+
+    for (int i = 0; i < 100; ++i) {
+      logger.write(Severity_level::INFO, "Test message " + std::to_string(i));
+      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+  } catch (const std::invalid_argument e) {
+    std::cerr << "Некорректный номер порта: не является числом" << std::endl;
+  } catch (std::runtime_error e) {
+    std::cerr << e.what() << std::endl;
+  }
+}
