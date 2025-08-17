252d1d01fea03f6e50ff274da75197679a8994e1
bebe-pirat <pirat.bebe@gmail.com>
2025-08-17 19:25:45 +0300

    Almost done

--- libs/logger/include/logger.h
+++ libs/logger/include/logger.h
@@ -10,6 +10,13 @@
 #include <stdexcept>
 #include <iomanip>
 #include <mutex>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <cerrno> 
 
 enum class Severity_level {
     DEBUG, 
@@ -17,9 +24,10 @@ enum class Severity_level {
     ERROR
 };
 
-std::string level_to_string(Severity_level level); // function for turning Severity level into string
-Severity_level string_to_level(std::string str); // function for turning string into Severity level
+std::string level_to_string(Severity_level level);
+Severity_level string_to_level(std::string str);
 
+// базовый класс ILogger
 class ILogger {
     private: 
         Severity_level level_;
@@ -28,9 +36,10 @@ public:
     virtual void write(Severity_level level, const std::string& text) = 0;
     void setLevel(Severity_level level);
     Severity_level getLevel() const;
-    std::string getMessage(Severity_level level, const std::string& text) const;
+    std::string getMessage(Severity_level level, const std::string& text) const; // метод для формирования строки лога
 };
 
+// дочерний класс для записи лога в файл
 class FileLogger : public ILogger {
     private: 
         std::string filename_;
@@ -40,6 +49,21 @@ class FileLogger : public ILogger {
         ~FileLogger();
         FileLogger(Severity_level level, std::string filename);
         void write(Severity_level level, const std::string& text) override;
+};
+
+// дочерний класс для записи лога в сокет
+class SocketLogger : public ILogger {
+    std::string host_;
+    unsigned short port_;
+    int sockfd_ = -1;
+    std::mutex mutex_;
+    
+    void connect_sock();
+    
+public:
+    SocketLogger(Severity_level level, unsigned short port, std::string host);
+    ~SocketLogger();
+    void write(Severity_level level, const std::string& text) override;
 };
 
 #endif  
--- libs/logger/src/logger.cpp
+++ libs/logger/src/logger.cpp
@@ -1,10 +1,10 @@
 #include "../include/logger.h"
 
+// функция для получения логга 
 std::string ILogger::getMessage(Severity_level level, const std::string& text) const {
     auto now = std::chrono::system_clock::now();
     std::time_t time_t_now = std::chrono::system_clock::to_time_t(now);
     std::tm local_tm{};
-
     localtime_r(&time_t_now, &local_tm);
 
     std::ostringstream oss;
@@ -13,6 +13,7 @@ std::string ILogger::getMessage(Severity_level level, const std::string& text) c
     return oss.str();
 }
 
+// функция для преобразования данной строки в уровень из перечисления 
 Severity_level string_to_level(std::string str) {
     static const std::map<std::string, Severity_level> levels_map = {
         {"DEBUG", Severity_level::DEBUG},
@@ -27,6 +28,7 @@ Severity_level string_to_level(std::string str) {
     return it->second;
 }
 
+// функция для преобразования уровня из перечисления в строку 
 std::string level_to_string(Severity_level level) {
     static const std::map<Severity_level, std::string> levels_map = {
         {Severity_level::DEBUG, "DEBUG"},
@@ -57,12 +59,68 @@ FileLogger::~FileLogger()
     out_.close();
 }
 
+// функция для записи логга в файл
 void FileLogger::write(Severity_level level, const std::string& text) {
     if (level < getLevel()) return;
     
     std::lock_guard<std::mutex> lock(mutex_); 
 
-    if (!out_.is_open()) throw std::runtime_error("Файл для записи не найден!");
+    if (!out_.is_open()) throw std::runtime_error("Файл для записи не открыт!");
 
     out_ << getMessage(level, text); 
+    out_.flush();
+}
+
+SocketLogger::SocketLogger(Severity_level level, unsigned short port, std::string host) : port_(port), host_(host)
+{
+    connect_sock();
+    setLevel(level);
+}
+
+// функция для создания и соединения
+void SocketLogger::connect_sock() {
+    sockfd_ = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd_ < 0) {
+        throw std::runtime_error("Сокет не удалось создать!");
+    }
+
+    sockaddr_in server_addr_;
+    server_addr_.sin_family = AF_INET;
+    server_addr_.sin_port = htons(port_);
+
+    if (inet_pton(AF_INET, host_.c_str(), &server_addr_.sin_addr) <= 0) {
+            close(sockfd_);
+            throw std::runtime_error("Неверный адрес!");
+        }
+
+
+    if (connect(sockfd_, (sockaddr*)&server_addr_, sizeof(server_addr_)) < 0) {
+        close(sockfd_);
+        throw std::runtime_error("Подключение не удалось совершить!");
+    }
+}
+
+SocketLogger::~SocketLogger() {
+    close(sockfd_);
+}
+
+// функция для записи логга в сокет
+void SocketLogger::write(Severity_level level, const std::string& text) {
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    if (sockfd_ < 0) {
+        try {
+            connect_sock();
+        }
+        catch(...) {
+            throw std::runtime_error("Переподключение не удалось совершить!");
+        }
+    }
+
+    std::string str = getMessage(level, text);
+    if (send(sockfd_, str.c_str(), str.size(), 0) < 0) {
+        close(sockfd_);
+        sockfd_ = -1;
+        throw std::runtime_error("Отправление не удалось совершить: " + std::string(strerror(errno))); 
+    }
 }
